86b895b835ac911c77e3b1b37bd348db7ef440d7
k971119 <hanyul97@naver.com>
2023-12-04 17:08:25 +0900

    채팅룸 UI 수정

--- src/main/resources/static/js/chatroom/chatRoomConnection.js
+++ src/main/resources/static/js/chatroom/chatRoomConnection.js
@@ -1,11 +1,7 @@
 let myStream;
-/*setTimeout(() => {
-    // 일단 소켓 연결을 해봐요
-    socket = new WebSocket('wss://192.168.45.93:443/socket/'+roomCode);
-})*/
 
 var socket = new WebSocket('wss://192.168.45.93:443/socket/'+roomCode);
-// 먼저 RTC객체를 만들어요 구글 stun 서버로 부터 나의 정보를 가져올게요
+// STUN서버 등록 및 RTC 객체생성
 var configuration = {
     "iceServers" : [{
         "url" : "stun:stun.l.google.com:19302"
@@ -32,9 +28,9 @@ function handleAddStream(data) {
     peerFace.srcObject = data.stream;
 }
 
-// 소켓이 연결되었을 때 실행할 콜백함수에요
+// 소켓 이벤트
 socket.onopen = function() {
-    console.log("웹소켓이 연결되었어요");
+    console.log("소켓 연결");
 };
 socket.onclose = (e) =>{
     console.log('소켓 닫힘');
@@ -42,45 +38,51 @@ socket.onclose = (e) =>{
 socket.onerror = function (e){
     console.log('소켓 연결 에러');
 }
-
-// 소켓에서 메세지를 받아왔을 때 실행할 콜백함수에요
 socket.onmessage = async function(msg) {
     var content = JSON.parse(msg.data);
     if (content.event == "offer") {
         console.log("오퍼가 왔어요");
-        // 오퍼가 오면 가장먼저 그 오퍼를 리모트 디스크립션으로 등록해줘요
+        //오퍼가 올경우 REMOTE 등록
         var offer = content.data;
         myPeerConnection.setRemoteDescription(offer);
-        // 받는 쪽에서도 자신의 미디어를 켜줘요
+
+        //오퍼를 받고 자신의 미디어를 열어준다.
         await getMedia();
         myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
-        // 이제 앤서를 보내요
+
+        //ANSWER 전송
         var answer = await myPeerConnection.createAnswer();
         myPeerConnection.setLocalDescription(answer);
-        console.log("앤서를 보낼게요");
+        console.log("ANSWER 전송");
         send({
             event: "answer",
             data: answer
         })
     } else if (content.event == "answer") {
-        console.log("앤서가 왔어요");
+        console.log("ANSWER RECEIVE");
         answer = content.data;
         myPeerConnection.setRemoteDescription(answer);
     } else if (content.event == "candidate") {
-        console.log("캔디데이트가 왔어요");
-        // 이 메서드를 통해 리모트 디스크립션에 설정되어있는 피어와의 연결방식을 결정해요
+        console.log("ICECANDIDATE RECEIVE");
+
+        //상대 클라이언트에 ICECANDIDATE 전송
         myPeerConnection.addIceCandidate(content.data);
     }
 }
 /*이벤트*/
 
-// 앞으로 소켓으로 메세지를 보낼 땐 이 함수를 쓸 생각이에요
+/**
+ * 시그널링 서버 전송용 함수
+ * @param message
+ */
 function send(message) {
     socket.send(JSON.stringify(message));
 }
 
-//미디어 내용을 받기 시작하는 함수에요
-//미디어와 오디오를 분리할 필요가 있어보임
+/**
+ * 미디어를 가져와서 VIDEO에 띄운후 전역 STREAM에 담는다
+ * @returns {Promise<unknown>}
+ */
 async function getMedia() {
     return new Promise(async (resolve, reject) => {
         try {
@@ -130,7 +132,7 @@ async function getMedia() {
     }
 }
 
-// '오퍼를 생성해요'라는 버튼을 눌렀을 때 이 메서드가 실행되요
+// 연결실행
 async function createOffer() {
 
     await getMedia();
